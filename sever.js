87b9911167c93f977b9e8eec8d895cf143e6ccfc
Matt Burns <matt.burns01@gmail.com>
2022-10-21 13:34:36 -0700

    created newNote variable and respond to json with newNote

--- sever.js
+++ sever.js
@@ -20,3 +20,21 @@ app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
+app.route("/api/notes")
+    .get(function(req, res){
+        res.json(database);
+    })
+
+    .post(function(req, res){
+        let jsonPath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        
+        fs.writeFile(jsonPath, JSON.stringify(database),function (err){
+            if (err){
+                return console.log(err);
+            }
+            console.log("saved note");
+        })
+        res.json(newNote);
+    })
